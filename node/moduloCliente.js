71870e0af9986d13265c25c3afe0b580e63a337a
Marcus Vinicius Mello Da Silva <vinicius.pro.contact@gmail.com>
2020-06-23 16:48:53 -0300

    Exportando módulo com commons JS

---
+++ node/moduloCliente.js
@@ -0,0 +1,13 @@
+// Importando um módulo
+
+const moduloA = require('./moduloa')
+
+const moduloB = require('./modulob')
+
+console.log(moduloA.ola)
+console.log(moduloA.ateLogo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
