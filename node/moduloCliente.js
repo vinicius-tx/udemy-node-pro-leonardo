9d40fd6b1c4cc3d8876c93f1a7538dc650d43e26
Marcus Vinicius Mello Da Silva <vinicius.pro.contact@gmail.com>
2020-06-23 16:51:22 -0300

    Correção para ficar completo

--- node/moduloCliente.js
+++ node/moduloCliente.js
@@ -5,9 +5,10 @@ const moduloA = require('./moduloa')
 const moduloB = require('./modulob')
 
 console.log(moduloA.ola)
-console.log(moduloA.ateLogo)
+console.log(moduloA.bemVindo)
 console.log(moduloA.ateLogo)
 console.log(moduloA)
 
 console.log(moduloB.bomDia)
 console.log(moduloB.boaNoite())
+console.log(moduloB)
